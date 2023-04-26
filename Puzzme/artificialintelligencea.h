f49de698440d9a33d6c588159ef8e389a24bf9c1
unknown <patryk.wojtiuk9@gmail.com>
2023-04-26 13:06:25 +0200

    detailed description

--- Puzzme/artificialintelligencea.h
+++ Puzzme/artificialintelligencea.h
@@ -27,22 +27,38 @@ private:
     std::vector<void (ArtificialIntelligenceA::*)(CellContainer*, int, int, int)> moveFunctions;
     /*!
      * \brief Sprawdza czy jest możliwość kliknięca na północną komórkę i klika.
+     * \param cellContainer kontener komórek.
+     * \param boardSize rozmiar planszy.
+     * \param rowIndex indeks wiersza wylosowanej pozycji.
+     * \param columnIndex indeks kolumny wylosowanej pozycji.
      */
     void moveNorth(CellContainer *cellContainer, int boardSize, int rowIndex, int columnIndex);
     /*!
      * \brief Sprawdza czy jest możliwość kliknięca na wschodnią komórkę i klika.
+     * \param boardSize rozmiar planszy.
+     * \param rowIndex indeks wiersza wylosowanej pozycji.
+     * \param columnIndex indeks kolumny wylosowanej pozycji.
      */
     void moveEast(CellContainer *cellContainer, int boardSize, int rowIndex, int columnIndex);
     /*!
      * \brief Sprawdza czy jest możliwość kliknięca na południową komórkę i klika.
+     * \param boardSize rozmiar planszy.
+     * \param rowIndex indeks wiersza wylosowanej pozycji.
+     * \param columnIndex indeks kolumny wylosowanej pozycji.
      */
     void moveSouth(CellContainer *cellContainer, int boardSize, int rowIndex, int columnIndex);
     /*!
      * \brief Sprawdza czy jest możliwość kliknięca na zachodnią komórkę i klika.
+     * \param boardSize rozmiar planszy.
+     * \param rowIndex indeks wiersza wylosowanej pozycji.
+     * \param columnIndex indeks kolumny wylosowanej pozycji.
      */
     void moveWest(CellContainer *cellContainer, int boardSize, int rowIndex, int columnIndex);
     /*!
      * \brief Wybiera komórkę w danym kontenerze o podanym indeksie oraz ją przemieszcza jeżeli sepłnia warunki przemieszczenia.
+     * \param boardSize rozmiar planszy.
+     * \param rowIndex indeks wiersza wylosowanej pozycji.
+     * \param columnIndex indeks kolumny wylosowanej pozycji.
      */
     void cellClick(CellContainer *cellContainer, int index);
 };
